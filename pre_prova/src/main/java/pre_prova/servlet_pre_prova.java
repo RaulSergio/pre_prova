639235884895d7c4d47973e7899602e9219c4d63
RaulSergio <dragolaymann@gmail.com>
2017-03-27 20:15:56 -0400

    criando operacoes

--- pre_prova/src/main/java/pre_prova/servlet_pre_prova.java
+++ pre_prova/src/main/java/pre_prova/servlet_pre_prova.java
@@ -19,6 +19,20 @@ public class servlet_pre_prova extends HttpServlet  {
 		String num2 = String.valueOf("Digite o Segundo Numero: ");
 		req.getParameter(num1);
 		req.getParameter(num2);
+		String op = req.getParameter("op");
+		double res;
+		
+		if(op.equals("som")){
+			res = Double.parseDouble(num1) + Double.parseDouble(num2);			
+		}else if(op.equals("sub")){
+			res = Double.parseDouble(num1) - Double.parseDouble(num2);
+		}else if(op.equals("mult")){
+			res = Double.parseDouble(num1) * Double.parseDouble(num2);
+		}else if(op.equals("div")){
+			res = Double.parseDouble(num1) / Double.parseDouble(num2);
+		}
+		
+		
 		 
 	}
 
